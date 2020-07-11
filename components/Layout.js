9c4c44e111fd1528a95dd44c6b2dd7c820c8512c
gmahima <mahimagangavarapu@gmail.com>
2020-07-11 15:36:51 +0530

    add heading style

--- components/Layout.js
+++ components/Layout.js
@@ -4,9 +4,11 @@ const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
+    text-align: center;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
       Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
       sans-serif;
+
   }
 `
 
@@ -23,6 +25,8 @@ const Layout = ({children}) => (
     </div>
 )
 const Heading = styled.h1`
+font-size: 2em;
+padding-top: 3em;
 text-align: center;
 
 `
