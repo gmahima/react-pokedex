c1d0fa35f048b1720b608e6a244935e0bb56dcf7
gmahima <mahimagangavarapu@gmail.com>
2020-07-12 11:42:16 +0530

    add styles

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
 import getAllPokemon from '../lib/data'
 import Layout from '../components/Layout'
+import styled from 'styled-components'
+
+const Container = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(100px, 150px));
+grid-gap: 30px;
+padding: 0 30px;
+justify-content: center;
+`
+
+const Card = styled.div`
+  display: grid;
+  box-shadow: 3px 3px 20px #DDD;
+  border-style: solid;
+  border-radius: 10px;
+  border-width: 1px;
+  justify-items: center;
+
+  
+
+`
+
+
 export default function Home({allPokemon}) {
   console.log(allPokemon)
   return (
 
       <Layout>
-        {allPokemon.results.map((p, i) => <PokeCard p={p} key={p.name} id={i+1}/>)}
+        <Container>
+          {allPokemon.results.map((p, i) => <PokeCard p={p} key={p.name} id={i+1}/>)}
+        </Container>
+        
       </Layout>
 
 
@@ -14,11 +40,11 @@ export default function Home({allPokemon}) {
 
 const PokeCard = ({p, id}) => (
 
-      <div>
+      <Card>
         <img src={`/sprites/${id}.png`} />
         <h4>{p.name}</h4>
         {console.log(p)}
-      </div>
+      </Card>
 
 )
 
