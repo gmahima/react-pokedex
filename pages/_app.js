0d8d53064342a8270080fc2c2dd78d76dfb1327d
gmahima <mahimagangavarapu@gmail.com>
2020-07-18 21:13:37 +0530

    add client side fetch with swr

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ import { createGlobalStyle } from 'styled-components'
 import React from 'react'
 import 'tailwindcss/dist/base.min.css'
 import tw, { css } from 'twin.macro'
+import useSWR from 'swr'
+import fetch from 'unfetch'
+ 
+const fetcher = url => fetch(url).then(r => r.json())
+ 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
@@ -13,12 +18,18 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <GlobalStyle />
-            <Component {...pageProps} />
-        </>
-    )
+  const { data, error } = useSWR("http://pokeapi.salestock.net/api/v2/pokedex/2/", fetcher)
+
+    // return (
+    //     <>
+    //         <GlobalStyle />
+            
+    //         <Component {...pageProps} />
+    //     </>
+    // )
+    if (error) return <div>failed to load</div>
+            if (!data) return <div>loading...</div>
+            return <div>{console.log(data)}!</div>
   }
 
   export default MyApp
